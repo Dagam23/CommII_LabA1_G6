1738446ae709004dd4bbce3143c6716d093b6f5b
Dagam23 <danielgamba2304@gmail.com>
2025-08-15 07:52:40 -0500

    Bloque Diferenciador y Acumulador

---
+++ Practica_1/GNURadio/untiled_epy_block_1.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+from gnuradio import gr
+
+
+class blk(gr.sync_block): 
+    def __init__(self, example_param=1.0):
+        gr.sync_block.__init__(
+            self,
+            name='e_Diff',
+            in_sig=[np.float32],
+            out_sig=[np.float32]
+        )
+    def work(self, input_items, output_items):
+        x = input_items[0]
+        y = output_items[0] 
+        
+        N = len(x)
+        diff = np.cumsum(x) - self.e_Acum
+
+        self.e_Acum = diff[N-1]
+
+        y0[:] = diff
+
+        return len(y)
+
